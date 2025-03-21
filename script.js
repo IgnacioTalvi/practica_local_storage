effd2147787da586581360fbb49a4b484d856ba8
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-03-21 18:15:32 +0100

    localstorage fix

--- script.js
+++ script.js
@@ -18,13 +18,83 @@ document.querySelector("form").addEventListener("submit", function (event) {
   if (message.length < 5) {
     alert("El mensaje debe contener mas de 5 caracteres");
     return;
-  } else storeLocalData(nameInput, email, message);
-  alert("Formulario enviado con éxito");
+  } else alert("Formulario enviado con éxito");
   event.target.submit();
+  storeLocalData(nameInput, email, message);
 });
 
-function storeLocalData(name, email, message) {
-  localStorage.setItem("name", name);
-  localStorage.setItem("email", email);
-  localStorage.setItem("message", message);
+function storeLocalData(nameInput, email, message) {
+  let userData = [{ nameInput, email, message }];
+  let newUserData = JSON.parse(localStorage.getItem("userData"));
+
+  localStorage.setItem("userData", JSON.stringify(userData));
+
+  // localStorage.setItem("userData", JSON.stringify(newUserData));
+
+  newUserData.push(userData);
+  localStorage.setItem("userData", JSON.stringify(newUserData));
 }
+
+// let bootcamps = ["fswd", "ux", "data", "ciber"];
+// localStorage.setItem("bootcamps", JSON.stringify(bootcamps));
+
+// let nuevoBootcamp = JSON.parse(localStorage.getItem("bootcamps"));
+
+// nuevoBootcamp.push("marketing");
+// localStorage.setItem("bootcamps", JSON.stringify(nuevoBootcamp));
+
+// function storeLocalData(nameInput, email, message) {
+//   addValue[i].addEventListener("click", () => {
+//     // get items from localStorage, or declare new one if not exist
+//     let userData = localStorage.getItem("UserData") || "[]";
+//     userData = JSON.parse(userData);
+//     // declare and add the new item
+//     userData.push({ name: nameInput.value, email: email.value, message: });
+//     localStorage.setItem("UserData", JSON.stringify(userData));
+//   });
+// }
+
+// addCart[i].addEventListener("click", () => {
+//   // get items from localStorage, or declare new one if not exist
+//   let menuItems = localStorage.getItem("ProductsInCart") || "[]";
+//   menuItems = JSON.parse(menuItems);
+//   // declare and add the new item
+//   menuItems.push({ name: addCart[i].name, price: addCart[i].value });
+//   localStorage.setItem("ProductsInCart", JSON.stringify(menuItems));
+// });
+
+// data.forEach((storeLocalData) => {
+//   return data;
+// });
+
+// const users = [
+//   {
+//     nameInput: "Ignacio",
+//     email: "Talvi",
+//     message: "Hola",
+//   },
+// ];
+
+// localStorage.setItem("name", nameInput);
+//   localStorage.setItem("email", email);
+//   localStorage.setItem("message", message);
+
+// let user = {
+//   name: "name",
+//   email: "email",
+//   message: "message",
+// };
+
+// let userString = JSON.parse(user);
+
+// let firstName = localStorage.getItem("name");
+// let lastName = localStorage.surname;
+// console.log(`Hola, mi nombre es ${firstName} ${lastName}`);
+
+//   let user = {
+//     name: name,
+//     email: email,
+//     age: age,
+//   };
+//   user;
+// }
